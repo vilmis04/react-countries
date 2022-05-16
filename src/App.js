36eb861c7112dde38ed7a3695cb0ce8c553a1667
Vilmantas Sudaris <vilmantas.sudaris@gmail.com>
2022-05-16 22:13:53 +0300

    feat: basic styles and list

--- src/App.js
+++ src/App.js
@@ -2,7 +2,31 @@ import { useState, useEffect } from 'react';
 
 function App() {
 
-return null;
+  const [countries, setCountries] = useState(undefined);
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v2/all?fields=name,region,area')
+      .then(res => res.json())
+      .then(data => setCountries(data))
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <>
+      {!countries && <h1>LOADING...</h1>}
+      {countries && 
+        <ul>
+          {countries.map(country => 
+            <li className='list-item' key={country.name}>
+              <p>Country: <strong>{country.name}</strong></p>
+              <p>Region: <strong>{country.region}</strong></p>
+              <p>Area: <strong>{country.area} km&#178;</strong></p>
+            </li>
+          )}
+        </ul>
+      }
+    </>
+  );
 }
 
 export default App;
